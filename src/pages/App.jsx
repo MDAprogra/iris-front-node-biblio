650cc70c60246462b5d8fb880f3689efeb848762
matthiasd <matth4627@gmail.com>
2025-12-09 10:43:00 +0100

    detele unuse import

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,3 @@
-import { useCallback, useEffect, useState } from 'react';
-
 import { Button } from '../components/Button';
 import { LivreItem } from '../components/LivreItem';
 import Title from '../components/title';
